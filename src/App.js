b376339e6f5b219421e484325412a33fb382b8dd
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-02-05 15:12:24 +0330

    add about page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 const Home = lazy(() => import('./components/Home'));
 const Contact = lazy(() => import('./components/Contact'));
+const About = lazy(() => import('./components/About'));
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/contact' element={<Contact />} />
+                    <Route path='/about' element={<About />} />
                 </Routes>
             </Suspense>
             <Footer />
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/About.scss';
+const About = () => {
+    return (
+        <div className='About'>
+            <h1>About me</h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Repellendus, quos!
+            </p>
+        </div>
+    );
+};
+
+export default About;
