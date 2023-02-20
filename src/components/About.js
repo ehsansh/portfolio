3f8ec2a7647a1b21c996ed11caef5c97c47bce1d
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-02-20 22:47:42 +0330

    add about me text

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,70 @@ const About = () => {
         <div className='About'>
             <h1>About me</h1>
             <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Repellendus, quos!
+                As this is the "About Me" page, I don't want to solely focus on
+                web development. It's about who I am as a person. Despite
+                studying physics in university, I'm a musician at heart and now
+                also a web developer. You might be wondering, how do these
+                things relate? Well, my first introduction to programming was
+                through a Fortran course in physics, and although it may not
+                seem like the most obvious connection, there are deeper ties
+                between problem-solving in physics and web development.
+                <br />
+                <br />
+                In physics, the language is mathematics, but it's not just a
+                collection of formulas. It's about understanding the question at
+                hand, analyzing data, and using problem-solving skills to find
+                solutions. I believe that this ability to approach problems
+                analytically is something that carries over into my work as a
+                web developer. For me, problem-solving is not just a job
+                requirement, but an enjoyable challenge.
+                <br />
+                <br />
+                How about exploring the relationship between music and web
+                development? At first glance, it might seem like an unlikely
+                pairing, but for me, they go hand in hand. As a musician turned
+                developer, I've created numerous web applications aimed at
+                teaching music, benefiting thousands of students along the way.
+                <br />
+                <br />
+                But my reasons for fusing music and development run deeper than
+                just a passion for both. As a developer, I understand that I'm
+                not just a code-robot, copy-pasting lines to create websites.
+                Rather, it's our creativity that sets us apart, allowing us to
+                bring value to our projects. And creativity isn't some fixed
+                quality that we're born with - it's something we can nurture and
+                develop. Studies suggest that playing with mud can boost
+                creativity in children, while for adults, experimenting with
+                musical instruments and notes can activate areas of the brain
+                associated with creativity.
+                <br />
+                <br />
+                In fact, I believe that leading a multi-dimensional life is a
+                recipe for creativity. When we allow ourselves to explore
+                different interests and pursuits, we begin to see connections
+                between seemingly unrelated things, opening the door to
+                innovation and discovery. It's why I'm passionate about blending
+                my love for music with my skills in development - because I know
+                that true creativity lies at the intersection of different
+                fields and experiences.
+                <br />
+                <br />
+                Let's return to the technical side of things. I began my journey
+                in web development in 2013 and gained experience working as a
+                PHP and front-end developer for small companies in 2014. In
+                2015, I embarked on my music startup project and have been
+                working on it since then while also taking on freelance
+                projects. You can find more information about my music endeavors
+                on my portfolio and view some of my coding projects on my GitHub
+                account.
+                <br />
+                <br />
+                Furthermore, I have completed numerous programming courses,
+                which can be found listed on my LinkedIn profile.
+                <br />
+                <br />
+                Thank you for taking the time to read through this page.
+                <br />
             </p>
         </div>
     );
